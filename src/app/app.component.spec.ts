25a81845cfc5daf458084d23680a1c555714ccc9
Martin Seibert <5963133+MartinSeibert@users.noreply.github.com>
2019-03-30 13:40:04 -0400

    added a test to the base app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,4 +28,12 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to mean-course!');
   });
+
+  it('should not have the title of \'hello\'', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).not.toContain('hello');
+
+  });
 });
