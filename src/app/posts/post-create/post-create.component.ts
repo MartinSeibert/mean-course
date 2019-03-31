ffc3a1849611b89ab4665cbe3f853c6a4d860ca4
Martin Seibert <5963133+MartinSeibert@users.noreply.github.com>
2019-03-30 23:09:00 -0400

    changed post creation to a form with error validation through ngForm

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
-import { Post } from './/post-model';
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -12,8 +13,14 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post: Post = { title: this.enteredTitle, content: this.enteredContent };
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
     this.postCreated.emit(post);
   }
 }
