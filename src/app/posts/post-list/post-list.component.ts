8669e6733609bd196fc57e8749cd9d4031eb0e97
Martin Seibert <5963133+MartinSeibert@users.noreply.github.com>
2019-03-30 23:39:17 -0400

    added a subject to the posts service and subscribed to it from post-list

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  @Input() posts: Post[] = [];
+export class PostListComponent implements OnInit {
+  posts: Post[] = [];
+
+  // relies on angulars' dependency injection to meet the postsService need
+  // due to the public keyword, the service can be referenced by this.postsService
+  constructor(public postsService: PostsService) {}
+
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+
+    this.postsService.getPostUpdateListener().subscribe((posts) => this.posts = posts);
+  }
+
+
 }
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,32 @@
+import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+// Angular decorator that allows for this class to be selected for dependency injection
+// the providedIn root parameter ensures that angular only creates one instance of this class
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  private posts: Post[] = [];
+
+  // defines a new subject to be used as an observable
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    // Spread operator ( [...arrayName] )creates a new array and adds all of the items from this.posts into the new array
+    return [...this.posts];
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { title, content };
+    this.posts.push(post);
+
+    // emits an observable has changed event for observers of postsUpdated to react to
+    this.postsUpdated.next([...this.posts]);
+  }
+
+  getPostUpdateListener()
+  {
+    // returns a reference to the Subject that can only be observed, not modified
+    return this.postsUpdated.asObservable();
+  }
+}
