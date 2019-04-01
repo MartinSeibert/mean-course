e4a73b0662348b5e0fbc88421c781e8afb8930ae
Martin Seibert <5963133+MartinSeibert@users.noreply.github.com>
2019-04-01 17:45:15 -0400

    Tied in the posts api to the post service

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 
 // read connectionstring in from outside of the repo to avoid pushing the password to github
-var mongoConnectionString = fs.readFileSync("../mongoConnectionString.txt").toString();
+var mongoConnectionString = fs
+  .readFileSync('../mongoConnectionString.txt')
+  .toString();
 
 console.log(mongoConnectionString);
 
@@ -15,16 +17,17 @@ const Post = require('./models/post.js');
 // Express apps are a chain of middleware functions
 const app = express();
 
-mongoose.connect(mongoConnectionString.toString()).then(()=>{
-  console.log('connected to database!');
-}).catch(() =>{
-  console.log('connection failed!');
-});
+mongoose
+  .connect(mongoConnectionString.toString())
+  .then(() => {
+    console.log('connected to database!');
+  })
+  .catch(() => {
+    console.log('connection failed!');
+  });
 
 // adds body parser to parse json data and make it available on the req object
 app.use(bodyParser.json());
-
-
 
 // this middleware allows for Cross Origin Resource Sharing so the api can be hit from outside of the server it is running on
 app.use((req, res, next) => {
@@ -40,37 +43,20 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) =>{
+app.post('/api/posts', (req, res, next) => {
   const post = new Post(req.body);
-  console.log(post);
-
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '1fsdafeqwqwr',
-      title: 'Title 1',
-      content: 'Content 1'
-    },
-    {
-      id: '2vasczx',
-      title: 'Title 2',
-      content: 'Content 2'
-    },
-    {
-      id: '3asdfaczx',
-      title: 'Title 3',
-      content: 'Content 3'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts sent successfully',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
   });
 });
 
