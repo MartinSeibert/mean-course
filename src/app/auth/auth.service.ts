f5a34a1f37537dab0681a0a2b6c39f60614b80d3
Martin Seibert <5963133+MartinSeibert@users.noreply.github.com>
2019-08-07 23:01:17 -0400

    login now returns a jwt token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   createUser(email: string, password: string) {
-// tslint:disable-next-line: object-literal-shorthand
-    const authData: AuthData = {email: email, password: password};
-    this.http.post('http://localhost:3000/api/user/signup', authData)
+    // tslint:disable-next-line: object-literal-shorthand
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    // tslint:disable-next-line: object-literal-shorthand
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         console.log(response);
       });
